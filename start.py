8a1600bd11a3f76d918adf14c71c32ff5cd3e13d
askar <adusadus498@gmail.com>
2024-12-24 00:50:25 +0100

    Update start.py
    Consol debug messages update

--- start.py
+++ start.py
@@ -8,7 +8,6 @@ from keep_alive import keep_alive
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logging.info(f"discord.py version: {discord.__version__}")
 
 
 load_dotenv()
@@ -18,11 +17,13 @@ class MyBot(commands.Bot):
     async def setup_hook(self):
         for extension in ['roll','ping','mimir','poke','auto_message','sun']:
             await self.load_extension(f'cogs.{extension}')
+            logging.info(f'Loaded: cogs.{extension}')
     async def on_ready(self):
         await bot.tree.sync()
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name =f"{bot.command_prefix}help"))
-        print(f'Lancé en tant que {self.user} !')
-        print(discord.__version__)
+        logging.info(f'Lancé en tant que {self.user} !')
+        logging.info(f"discord.py version: {discord.__version__}")
+        
 
 intents = discord.Intents.all()
 bot = MyBot(command_prefix='.', intents=intents)
