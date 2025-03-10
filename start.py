b5aada0676f655e9e7cd68da032f5aad9f9c9793
askar <adusadus498@gmail.com>
2025-03-10 19:38:01 +0100

    Update start.py

--- start.py
+++ start.py
@@ -15,7 +15,7 @@ token = os.getenv('DISCORD_TOKEN')
 
 class MyBot(commands.Bot):
     async def setup_hook(self):
-        for extension in ['random','ping','mimir','poke','sun','messages','bug_report','genance','status','events','absences.py']:
+        for extension in ['random','ping','mimir','poke','sun','messages','bug_report','genance','status','events','absences']:
             await self.load_extension(f'cogs.{extension}')
             logging.info(f'Loaded: cogs.{extension}')
     async def on_ready(self):
